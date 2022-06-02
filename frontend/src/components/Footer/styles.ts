ac3bc8da95fc4839ee53cafae0e1268bf64e05da
WesleyEstevam <98910359+WesleyEstevam@users.noreply.github.com>
2022-06-01 21:53:12 -0300

    Pasta frontend deletada

--- frontend/src/components/Footer/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  padding: 1.2rem 0.6rem 2rem 0.6rem;
-  text-align: center;
-`;
--- frontend/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from '../Context/AuthContext';
-import { PublicRoutes } from './public.routes';
-import { PrivateRoutes } from './private.routes';
-
-export function Routes() {
-  const { isAuthenticated } = useAuth();
-
-  return isAuthenticated ? <PrivateRoutes /> : <PublicRoutes />;
-}
